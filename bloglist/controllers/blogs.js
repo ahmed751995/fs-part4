cd48cdc582b8917a42848f8b3ce74e617dce3833
Ahmed Abdulrahman <ahmed751995@riseup.net>
2023-04-01 03:23:44 +0200

    Exercises 4.13.-4.14.

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -7,15 +7,40 @@ blogRoute.get("/", async (request, response) => {
 });
 
 blogRoute.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
+  if (body.title && body.url) {
+    const blog = new Blog(body);
+    if (!blog.likes) blog.likes = 0;
+    const result = await blog.save();
+    response.status(201).json(result);
+  }
+  response.status(400).end();
+});
 
-  if (!blog.title || !blog.url) response.status(400).end();
+blogRoute.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
 
-  if (!blog.likes) blog.likes = 0;
+blogRoute.put("/:id", async (request, response) => {
+  const body = request.body;
 
-  const result = await blog.save();
+  const blog = {
+    title: body.title,
+    url: body.url,
+    author: body.author,
+    likes: body.likes,
+  };
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+    context: "query",
+  });
 
-  response.status(201).json(result);
+  if(updatedBlog) {
+    response.json(updatedBlog);
+  }
+  response.status(404).end();
+
 });
 
 module.exports = blogRoute;
--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require("supertest");
 const Blog = require("../models/blog");
 const helper = require("./test_helper");
 const app = require("../app");
+const { update } = require("lodash");
 const api = supertest(app);
 
 beforeEach(async () => {
@@ -53,10 +54,9 @@ test("adding new blog", async () => {
   const resp = await api.post("/api/blogs").send(blog).expect(201);
 
   const newBlogs = await helper.blogsInDb();
-  
+
   const postBlog = resp.body;
 
-  
   expect(newBlogs).toHaveLength(oldBlogs.length + 1);
   expect(postBlog.title).toEqual(blog.title);
 });
@@ -65,29 +65,65 @@ test("likes is existed if not should be 0", async () => {
   const blog = {
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
   };
 
   const resp = await api.post("/api/blogs").send(blog).expect(201);
 
   const newBlog = resp.body;
-  
+
   expect(newBlog.likes).toBeDefined();
   expect(newBlog.likes).toBe(0);
 });
 
-
 test("test existance of url and title", async () => {
   const blogWithoutTitle = {
     author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
   };
 
-  const blogWithoutUrl =  {
+  const blogWithoutUrl = {
     title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra"
+    author: "Edsger W. Dijkstra",
   };
-
   await api.post("/api/blogs").send(blogWithoutTitle).expect(400);
-    await api.post("/api/blogs").send(blogWithoutUrl).expect(400);
+  await api.post("/api/blogs").send(blogWithoutUrl).expect(400);
+});
+
+describe("deleting blog", () => {
+  test("removing first item", async () => {
+    const notesBeforeDelete = await helper.blogsInDb();
+    const deletedBlog = notesBeforeDelete[0];
+    await api.delete(`/api/blogs/${deletedBlog.id}`).expect(204);
+    const notesAfterDelete = await helper.blogsInDb();
+
+    expect(notesAfterDelete).toHaveLength(notesBeforeDelete.length - 1);
+  });
+});
+
+describe("updating blogs", () => {
+  test("updating single blog", async () => {
+    const blog = (await helper.blogsInDb())[0];
+    blog.url = "google.com";
+    const response = await api.put(`/api/blogs/${blog.id}`).send(blog);
+
+    const blogAfterUpdate = response.body;
+    const blogs = await helper.blogsInDb();
+    
+    const updatedBlog = blogs.filter(b => b.id === blogAfterUpdate.id)[0];
+    expect(updatedBlog.url).toBe("google.com");
+  });
+
+  test("update blog doesn't exist", async () => {
+    const blog = new Blog();
+    blog.url = "start.com";
+    blog.id = "2131lksdf";
+
+    await api.put(`/api/blogs/${blog.id}`).send(blog).expect(404);
+    
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
 });
